695f5ff6521a58d722c3805bc37c950d58e6a5e7
Andres Granados <afgranados27@gmail.com>
2025-01-16 21:18:37 -0500

    login

---
+++ src/app/guards/intro.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+
+
+@Injectable({providedIn: 'root'})
+export class IntroGuard implements CanActivate {
+
+constructor(private storage: Storage, private router: Router) {}
+
+  async canActivate() {
+
+    return await this.storage.get('viLaIntro') ?? 
+    this.router.navigateByUrl('/intro');
+  }
+}
--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -9,12 +10,17 @@ import { Router } from '@angular/router';
 })
 export class IntroPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private storage: Storage
+  ) { }
 
   ngOnInit() {
   }
 
   finish() {
+
+    this.storage.set('viLaIntro', true);
+    console.log('Intro finished');
     this.router.navigateByUrl('/home');
   }
 
